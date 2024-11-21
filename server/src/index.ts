aeb5dbcccb4cd25671ed2d75dd1368a943d758ce
asirialwis <asirichathurika900@gmail.com>
2024-11-21 20:21:15 +0530

    update server port to 5000 and configure CORS middleware for enhanced security

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,9 +7,15 @@ import authRoutes from "./routes/authRoutes";
 import contactRoutes from "./routes/contactRoutes";
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allow required methods
+  allowedHeaders: ['Content-Type', 'Authorization'],   // Allow required headers
+}));
+
 
 // Middleware to parse JSON
 app.use(express.json());
