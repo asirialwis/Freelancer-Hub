ce8d983533c19855268c916c67ee2354034d8a10
asirialwis <asirichathurika900@gmail.com>
2024-11-22 10:24:31 +0530

    refactor authenticateToken middleware to improve token validation and enhance error handling

--- server/src/middleware/authenticateToken.ts
+++ server/src/middleware/authenticateToken.ts
@@ -5,20 +5,29 @@ export default async function authenticateToken(req: any, res: any, next: any) {
     const authHeader = req.headers["authorization"];
     const accessToken = authHeader && authHeader.split(" ")[1];
 
-    if(!accessToken){
+    if (!accessToken) {
         return res.status(401).json({
             success: false,
             message: "Access Denied: No token Provided",
         });
     }
-   
-    try{
+
+    try {
         const decoded = verify(accessToken, process.env.JWT_SECRET_ACCESS as string);
-        req.accessToken = {value: accessToken};
-        req.user = decoded;
-        next();
+
+        if (typeof decoded === "object" && "userId" in decoded) {
+            req.accessToken = { value: accessToken };
+            req.user = decoded;
+            req.userId = decoded.userId; // Now TypeScript knows userId exists
+            next();
+        } else {
+            return res.status(403).json({
+                success: false,
+                message: "Invalid or Malformed Token",
+            });
+        }
     }
-    catch(err){
+    catch (err) {
         console.log(err);
         return res.status(403).json({
             success: false,
