9bebdc95a32eddf9c62351b7b24a1ac8fd481c5e
asirialwis <asirichathurika900@gmail.com>
2024-11-23 12:58:14 +0530

    intial v1

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,13 +3,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Home from "./components/Home";
 import ContactForm from "./components/ContactForm";
+import SignUp from "./components/SignUp";
 import PrivateRoute from "./components/PrivateRoute";
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
-
+        <Route path= "/signup" element={<SignUp/>}></Route>
         <Route path="/login" element={<Login />} />
         <Route path="/home" element = {<Home/>}></Route>
         <Route
